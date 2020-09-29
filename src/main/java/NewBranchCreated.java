3ad82dd988da7652ef4d904514621ccf22d3ae99
Caofeifei <1>
2020-09-29 19:14:44 +0800

    new Branch

---
+++ src/main/java/NewBranchCreated.java
@@ -0,0 +1,9 @@
+/**
+ * @author Parker Cao
+ * @create 2020-09-29 19:13
+ */
+public class NewBranchCreated {
+    public static void main(String[] args) {
+        System.out.println("111111111111");
+    }
+}
