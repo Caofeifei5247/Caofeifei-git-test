d4d8745439b32b22dd7a3e22d038c08bac860db0
parker <545431131@qq.com>
2020-09-29 18:48:55 +0800

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author Parker Cao
+ * @create 2020-09-29 18:47
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloWorld");
+    }
+}
