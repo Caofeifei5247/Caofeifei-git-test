545f7cd370b1f2c6792d2292f4c0c96f1aa03187
ramon <2>
2020-09-29 19:20:11 +0800

    ramom change3

---
+++ src/main/java/NewBranch2.java
@@ -0,0 +1,9 @@
+/**
+ * @author Parker Cao
+ * @create 2020-09-29 19:13
+ */
+public class NewBranch2 {
+    public static void main(String[] args) {
+        System.out.println("2222222222222");
+    }
+}
