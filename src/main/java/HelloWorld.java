b1f7f2cd9ad6b2ec6d11a125395efc3dabf5a09c
ramon <1>
2020-09-29 19:11:17 +0800

    ramom change2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,9 +6,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloWorld");
         System.out.println("helloWorld");
-        System.out.println("helloWorld");
-        System.out.println("helloWorld");
-        System.out.println("helloWorld");
-        System.out.println("helloWorld");
     }
 }
