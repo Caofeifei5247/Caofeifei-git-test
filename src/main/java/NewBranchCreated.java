990b9d1d6b74cd334606af7b9997395d67b4574b
Parker <caofeifei5247@163.com>
2021-01-17 23:35:31 +0800

    delete

--- src/main/java/NewBranchCreated.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Parker Cao
- * @create 2020-09-29 19:13
- */
-public class NewBranchCreated {
-    public static void main(String[] args) {
-        System.out.println("112212222111111111");
-    }
-}
